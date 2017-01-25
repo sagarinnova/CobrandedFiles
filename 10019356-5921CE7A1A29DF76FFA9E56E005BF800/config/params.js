aa387656284595f4f78a51b0d2f99a78f550096b
Sagar Venkatesh Das <sdas1@rws-mbp-101.local>
2017-01-25 15:22:53 -0800

    first commit

---
+++ 10019356-5921CE7A1A29DF76FFA9E56E005BF800/config/params.js
@@ -0,0 +1,42 @@
+exports.themes ={'10003600':{theme:'fl','modules':{'10003591':'fl', '10003592':'fl','10003593':'fl','10003594':'fl','10003595':'fl','10003596':'fl','10003597':'fl','10003598':'fl'}}};
+exports['10003600'] = {
+    overrides : {
+    	newUserExperienceEnabledForFL : true
+    }
+};
+///////////////////////////////////////////////////
+/////// GENERAL SHOW/HIDE LINKS ///////////////////
+///////////////////////////////////////////////////
+// Show / hide My Accounts section link
+exports.showMyAccounts = false; //product recommendation to keep it off
+exports.popularSitesEnabled = true;
+exports.suggestedSitesEnabled = true;
+exports.enableOtherAccountsSection = true;
+exports.addManualAccountEnabled = true;
+exports.addRealestateAccountEnabled = true;
+exports.siteLevelTooltipEnabled = false;
+exports.showBreadcrumb = true;
+exports.reenterPasswordFieldEnabled = false;
+exports.showTypeEnabled = true;
+exports.addSiteAccountCredentailsHistoryEnabled = false;
+exports.addSiteCredentailsTncEnabled = false;
+exports.addSiteCredentailsTncVersion = 0;
+exports.showAddAccountButton = true;
+exports.multiAccountAdditionEnabled = true;
+exports.settingsEditOptionEnabled = true;
+exports.settingsDeleteOptionEnabled = true;
+exports.showIncludeNetworthForRealestate = true;
+exports.skipMortgageAccountPageRealestate = false;
+
+// keys for new theme
+exports.footerSectionEnabledForNewFL= true;
+exports.showFooterContentInPageForNewFL= true;
+exports.showMyAccountSectionInNewTheme = true;
+exports.showFormFieldlabel = true;
+exports.showSplashPage = false;
+exports.HDlogoFeatureEnabled = true;
+/////////////////////////////////////////////////////////////////////
+///// CUSTOM PARAMS ///////
+/////////////////////////////////////////////////////////////////////
+
+//Enter custom parameters here;
